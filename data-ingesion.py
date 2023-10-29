f037b18531dedfccb31b204a32ea856cc905dece
Karpagapriyad <karpagapriyad62@gmail.com>
2023-10-29 13:25:13 +0100

    Written a function to split the dataset in to different datasets and stored in csv file

---
+++ data-ingesion.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
+
+
+def save_breast_cancer_data(num_data_points_per_file):
+    data = load_breast_cancer()
+    X, y = data.data, data.target
+    num_csv_files = len(X) // num_data_points_per_file
+    for i in range(num_csv_files):
+        start_idx = i * num_data_points_per_file
+        end_idx = (i + 1) * num_data_points_per_file
+        subset_X = X[start_idx:end_idx]
+        subset_y = y[start_idx:end_idx]
+        df = pd.DataFrame(data=subset_X, columns=data.feature_names)
+        df['target'] = subset_y
+        file_name = f'breast_cancer_data_{i + 1}.csv'
+        df.to_csv(file_name, index=False)
+        print(f'Saved {num_data_points_per_file} data points in {file_name}')
+
+
+
+
+
+save_breast_cancer_data(num_data_points_per_file=100)
