e1fc32f923b178b8d63fae7c751dfecf90281d9a
Mona Lisa <vicgen2019@gmail.com>
2023-11-02 20:10:16 +0100

    updated version of API to take bulk or single stream data

--- api_main.py
+++ api_main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import joblib
 import psycopg2
@@ -21,11 +21,32 @@ class Features(BaseModel):
 class PredictionResponse(BaseModel):
     prediction: str
 
-class PastPredictionResponse(BaseModel):
-    past_predictions: list
+class PastPrediction(BaseModel):
+    features: Features
+    prediction: str
 
 @app.post('/predict', response_model=PredictionResponse)
-def predict(features: Features):
+def predict_single(features: Features):
+    return predict([features])
+
+@app.post('/bulk_predict', response_model=PredictionResponse)
+def predict_bulk(features_list: List[Features]):
+    return predict(features_list)
+
+def predict(features):
+    if isinstance(features, list):
+        # Handle bulk prediction
+        predictions = []
+        for feature in features:
+            prediction = make_prediction(feature)
+            predictions.append(prediction)
+        return {"prediction": predictions}
+    else:
+        # Handle single prediction
+        prediction = make_prediction(features)
+        return {"prediction": prediction}
+
+def make_prediction(features):
     # Convert features to a list for model prediction
     features_list = [features.mean_radius, features.mean_texture, features.mean_perimeter, features.mean_area]
     
@@ -35,29 +56,19 @@ def predict(features: Features):
     # Map prediction to 'benign' or 'malignant'
     prediction_label = 'benign' if prediction == 0 else 'malignant'
 
-    # Save prediction and features to PostgreSQL
-    cursor.execute("INSERT INTO predictions (mean_radius, mean_texture, mean_perimeter, mean_area, prediction) VALUES (%s, %s, %s, %s, %s)", (features.mean_radius, features.mean_texture, features.mean_perimeter, features.mean_area, prediction_label))
+    # Save features and prediction to PostgreSQL
+    cursor.execute("INSERT INTO past_predictions (mean_radius, mean_texture, mean_perimeter, mean_area, prediction) VALUES (%s, %s, %s, %s, %s)", (features.mean_radius, features.mean_texture, features.mean_perimeter, features.mean_area, prediction_label))
     conn.commit()
 
-    return {"prediction": prediction_label}
+    return prediction_label
 
-@app.get('/past_predictions', response_model=PastPredictionResponse)
-def past_predictions():
-    # Retrieve past predictions and features from the database
-    cursor.execute("SELECT mean_radius, mean_texture, mean_perimeter, mean_area, prediction FROM predictions")
+@app.get('/past_predictions', response_model=List[PastPrediction])
+def get_past_predictions():
+    cursor.execute("SELECT mean_radius, mean_texture, mean_perimeter, mean_area, prediction FROM past_predictions")
     past_predictions = []
     for row in cursor.fetchall():
-        features = {
-            'mean_radius': row[0],
-            'mean_texture': row[1],
-            'mean_perimeter': row[2],
-            'mean_area': row[3]
-        }
+        features = Features(mean_radius=row[0], mean_texture=row[1], mean_perimeter=row[2], mean_area=row[3])
         prediction_label = row[4]
-        past_predictions.append({'features': features, 'prediction': prediction_label})
+        past_predictions.append({"features": features, "prediction": prediction_label})
 
-    return {"past_predictions": past_predictions}
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    return past_predictions
